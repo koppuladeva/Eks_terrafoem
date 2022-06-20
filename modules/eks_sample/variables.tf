f4f8b20c4cbb47b24c69e5793b867700bd06b081
koppuladeva <57366811+koppuladeva@users.noreply.github.com>
2022-06-20 11:35:39 +0200

    Add files via upload

---
+++ modules/eks_sample/variables.tf
@@ -0,0 +1,46 @@
+# EKS Cluster Input Variables
+variable "cluster_name" {
+  description = "Name of the EKS cluster. Also used as a prefix in names of related resources."
+  type        = string
+  default     = "eks_gitlab_runner_cluster"
+}
+
+variable "cluster_service_ipv4_cidr" {
+  description = "service ipv4 cidr for the kubernetes cluster"
+  type        = string
+  default     = null
+}
+
+variable "cluster_version" {
+  description = "Kubernetes minor version to use for the EKS cluster "
+  type        = string
+  default     = null
+}
+variable "cluster_endpoint_private_access" {
+  description = "Indicates whether or not the Amazon EKS private API server endpoint is enabled."
+  type        = bool
+  default     = false
+}
+
+variable "cluster_endpoint_public_access" {
+  description = "Indicates whether or not the Amazon EKS public API server endpoint is enabled. When it's set to `false` ensure to have a proper private access with `cluster_endpoint_private_access = true`."
+  type        = bool
+  default     = true
+}
+
+variable "cluster_endpoint_public_access_cidrs" {
+  description = "List of CIDR blocks which can access the Amazon EKS public API server endpoint."
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+# EKS Node Group Variables
+## Placeholder space you can create if required
+
+
+# cluster_name = "eks_gitlab_runner_cluster"
+# cluster_service_ipv4_cidr = "172.20.0.0/16"
+# cluster_version = "1.22"
+# cluster_endpoint_private_access = true
+# cluster_endpoint_public_access = true
+# cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
